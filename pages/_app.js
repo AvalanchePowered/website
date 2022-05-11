2e0215944766f8633e62ec08959b90488dde6105
RealTriassic <techbyteofficial9@gmail.com>
2022-05-11 11:51:42 +0300

    Update .gitignore

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import { DefaultSeo } from 'next-seo';
-import ReactTooltip from 'react-tooltip';
 import SEO from '../nextseo.config';
 import AOS from 'aos';
 
@@ -24,7 +23,6 @@ function App({ Component, pageProps }) {
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       <DefaultSeo {...SEO} />
-      <ReactTooltip backgroundColor="black" textColor="white" />
       <Component {...pageProps} />
     </>
   )
